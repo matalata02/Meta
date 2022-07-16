7b2d3f0615cdafc8f2fc78670e1586b1b10d8b26
matalata021 <84315532+matalata02@users.noreply.github.com>
2022-07-16 13:58:38 +0700

    Update Playlist.py

--- Yukki/Plugins/Playlist.py
+++ Yukki/Plugins/Playlist.py
@@ -3,14 +3,14 @@ from pyrogram.types import (InlineKeyboardButton, InlineKeyboardMarkup,
                             KeyboardButton, Message, ReplyKeyboardMarkup,
                             ReplyKeyboardRemove)
 
-from Yukki import BOT_ID, BOT_USERNAME, MUSIC_BOT_NAME, SUDOERS, app, db_mem
-from Yukki.Database import (_get_playlists, delete_playlist, get_playlist,
+from Meta import BOT_ID, BOT_USERNAME, MUSIC_BOT_NAME, SUDOERS, app, db_mem
+from Meta.Database import (_get_playlists, delete_playlist, get_playlist,
                             get_playlist_names, save_playlist)
-from Yukki.Decorators.admins import AdminRightsCheck
-from Yukki.Decorators.assistant import AssistantAdd
-from Yukki.Decorators.checker import checker, checkerCB
-from Yukki.Decorators.permission import PermissionCheck
-from Yukki.Inline import (add_genre_markup, check_genre_markup, check_markup,
+from Meta.Decorators.admins import AdminRightsCheck
+from Meta.Decorators.assistant import AssistantAdd
+from Meta.Decorators.checker import checker, checkerCB
+from Meta.Decorators.permission import PermissionCheck
+from Meta.Inline import (add_genre_markup, check_genre_markup, check_markup,
                           delete_playlist_markuup, download_markup,
                           others_markup, play_genre_playlist, playlist_markup,
                           third_playlist_markup)
@@ -28,11 +28,11 @@ __HELP__ = """
 
 
 /delmyplaylist
-- Delete any saved music in your playlist
+- Delete any saved in your playlist
 
 
 /delgroupplaylist
-- Delete any saved music in your group's playlist [Requires Admin Rights.]
+- Delete any saved in your group's playlist [Requires Admin Rights.]
 """
 
 
