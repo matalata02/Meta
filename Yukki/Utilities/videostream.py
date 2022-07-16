761ed0d6dc58f535726f3f04fc2861ecc3a8e54e
matalata021 <84315532+matalata02@users.noreply.github.com>
2022-07-16 13:39:48 +0700

    Update videostream.py

--- Yukki/Utilities/videostream.py
+++ Yukki/Utilities/videostream.py
@@ -7,16 +7,16 @@ from pyrogram.types import InlineKeyboardMarkup
 from pyrogram.types.messages_and_media import message
 
 from config import get_queue
-from Yukki import BOT_USERNAME, db_mem
-from Yukki.Core.PyTgCalls import Queues
-from Yukki.Core.PyTgCalls.Yukki import (join_live_stream, join_video_stream,
+from Meta import BOT_USERNAME, db_mem
+from Meta.Core.PyTgCalls import Queues
+from Meta.Core.PyTgCalls.Meta import (join_live_stream, join_video_stream,
                                         stop_stream)
-from Yukki.Database import (add_active_chat, add_active_video_chat,
+from Meta.Database import (add_active_chat, add_active_video_chat,
                             is_active_chat, music_off, music_on,
                             remove_active_chat)
-from Yukki.Inline import (audio_markup, audio_markup2, primary_markup,
+from Meta.Inline import (audio_markup, audio_markup2, primary_markup,
                           secondary_markup, secondary_markup2)
-from Yukki.Utilities.timer import start_timer
+from Meta.Utilities.timer import start_timer
 
 loop = asyncio.get_event_loop()
 
