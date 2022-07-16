0071a44d909a07493712ad6e25a4dcc02285457d
matalata021 <84315532+matalata02@users.noreply.github.com>
2022-07-16 14:04:02 +0700

    Update Auth.py

--- Yukki/Plugins/Auth.py
+++ Yukki/Plugins/Auth.py
@@ -1,12 +1,12 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-from Yukki import SUDOERS, app
-from Yukki.Database import (_get_authusers, delete_authuser, get_authuser,
+from Meta import SUDOERS, app
+from Meta.Database import (_get_authusers, delete_authuser, get_authuser,
                             get_authuser_count, get_authuser_names,
                             save_authuser)
-from Yukki.Decorators.admins import AdminActual
-from Yukki.Utilities.changers import (alpha_to_int, int_to_alpha,
+from Meta.Decorators.admins import AdminActual
+from Meta.Utilities.changers import (alpha_to_int, int_to_alpha,
                                       time_to_seconds)
 
 __MODULE__ = "Auth Users"
