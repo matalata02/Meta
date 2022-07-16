6de938f833a58373270a6a6276cfe8441f5b13f0
matalata021 <84315532+matalata02@users.noreply.github.com>
2022-07-16 13:56:08 +0700

    Update Speedtest.py

--- Yukki/Plugins/Speedtest.py
+++ Yukki/Plugins/Speedtest.py
@@ -5,8 +5,8 @@ import wget
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-from Yukki import BOT_ID, SUDOERS, app
-from Yukki.Utilities.formatters import bytes
+from Meta import BOT_ID, SUDOERS, app
+from Meta.Utilities.formatters import bytes
 
 
 @app.on_message(filters.command("speedtest") & ~filters.edited)
