9e6263c4c63d01c3aaa7a9e6eb161a2514fedddf
matalata021 <84315532+matalata02@users.noreply.github.com>
2022-07-16 21:55:42 +0700

    Update __main__.py

--- Meta/__main__.py
+++ Meta/__main__.py
@@ -94,12 +94,12 @@ async def initiate_bot():
             status="[bold blue]Importation Completed!",
         )
     console.print(
-        "[bold green]Congrats!! Yukki Music Bot has started successfully!\n"
+        "[bold green]Congrats!! Mate has started successfully!\n"
     )
     try:
         await app.send_message(
             LOG_GROUP_ID,
-            "<b>Congrats!! Music Bot has started successfully!</b>",
+            "<b>Congrats!! Mate has started successfully!</b>",
         )
     except Exception as e:
         print(
