b67f56d2fc322641b5f986860776ff7117a1ffcb
matalata021 <84315532+matalata02@users.noreply.github.com>
2022-07-18 10:53:35 +0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #    Credits @FFmpegNotInstalled
-#    Credits @FFmpegNotInstalled
-#    Credits @FFmpegNotInstalled
+#    Credits @matalata02
 
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
